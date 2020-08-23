07b8d8c11824e0efaa9d38fa7698ac8c076afefa
Senior S <maximatador23@gmail.com>
2020-08-23 19:57:34 -0300

    1.0.0
    1.0.0

---
+++ BuilderMode.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Cysharp.Threading.Tasks;
+using OpenMod.Unturned.Plugins;
+using OpenMod.API.Plugins;
+using System.Collections.Generic;
+using SDG.Unturned;
+using UnityEngine;
+using OpenMod.API.Users;
+using OpenMod.Core.Helpers;
+using OpenMod.Core.Users;
+using OpenMod.Unturned.Users;
+
+[assembly: PluginMetadata("SS.BuilderMode", DisplayName = "BuilderMode")]
+namespace BuilderMode
+{
+    public static class PlayerManager
+    {
+        public static List<string> InBuilderMode = new List<string>();
+    }
+
+    public class BuilderMode : OpenModUnturnedPlugin
+    {
+        private readonly IUserManager ro_UserManager;
+        private readonly ILogger<BuilderMode> ro_Logger;
+
+        public BuilderMode(
+            IUserManager userManager, 
+            ILogger<BuilderMode> logger, 
+            IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            ro_UserManager = userManager;
+            ro_Logger = logger;
+        }
+
+        protected override async UniTask OnLoadAsync()
+        {
+            await UniTask.SwitchToMainThread();
+            BarricadeManager.onDeployBarricadeRequested += OnDeployBarricade;
+            ro_Logger.LogInformation("Plugin loaded correctly!");
+            ro_Logger.LogInformation("If you have any error you can contact the owner in discord: Senior S#9583");
+        }
+
+        private void OnDeployBarricade(Barricade barricade, ItemBarricadeAsset asset, Transform hit, ref Vector3 point, ref float angle_x, ref float angle_y, ref float angle_z, ref ulong owner, ref ulong group, ref bool shouldAllow)
+        {
+            var z = owner;
+            shouldAllow = true;
+            AsyncHelper.RunSync(async () => {
+                var user = (UnturnedUser)await ro_UserManager.FindUserAsync(KnownActorTypes.Player, z.ToString(), UserSearchMode.FindById);
+                var x = PlayerManager.InBuilderMode;
+                if (x.Contains(user.Id))
+                {
+                    var c = new Item(asset.id, true);
+                    user.Player.inventory.tryAddItem(c, true);
+                }
+            });
+        }
+
+        protected override async UniTask OnUnloadAsync()
+        {
+            await UniTask.SwitchToMainThread();
+            BarricadeManager.onDeployBarricadeRequested -= OnDeployBarricade;
+            ro_Logger.LogInformation("Plugin unloaded correctly!");
+        }
+    }
+}
---
+++ Commands.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using OpenMod.Core.Commands;
+using System.Threading.Tasks;
+using OpenMod.Unturned.Users;
+using Microsoft.Extensions.Localization;
+
+namespace BuilderMode
+{
+    [Command("buildermode")]
+    [CommandAlias("bm")]
+    [CommandAlias("bmode")]
+    [CommandDescription("Command to enable or disable the builder mode.")]
+    public class CommandAwesome : Command
+    {
+        private readonly IStringLocalizer ro_StringLocalizer;
+
+        public CommandAwesome(IServiceProvider serviceProvider, IStringLocalizer stringLocalizer) : base(serviceProvider)
+        {
+            ro_StringLocalizer = stringLocalizer;
+        }
+
+        protected override async Task OnExecuteAsync()
+        {
+            var user = (UnturnedUser)Context.Actor;
+            var z = PlayerManager.InBuilderMode;
+            if (z.Contains(user.Id))
+            {
+                z.Remove(user.Id);
+                await user.PrintMessageAsync(ro_StringLocalizer["plugin_translations:buildermode_off"], System.Drawing.Color.Red);
+            }
+            else
+            {
+                z.Add(user.Id);
+                await user.PrintMessageAsync(ro_StringLocalizer["plugin_translations:buildermode_on"], System.Drawing.Color.Blue);
+            }
+        }
+    }
+}
